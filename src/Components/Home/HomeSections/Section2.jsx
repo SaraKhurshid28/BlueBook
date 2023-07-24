a2c0f0366781eac33e48d9913820bd558bbb5513
Sara Khurshid <Sara.Khurshid@zweidevs.com>
2023-07-24 21:33:43 +0500

    change the useEffect

--- src/Components/Home/HomeSections/Section2.jsx
+++ src/Components/Home/HomeSections/Section2.jsx
@@ -143,7 +143,7 @@ const Section2 = () => {
   useEffect(() => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  });
+  }, []);
 
   return (
     <>
