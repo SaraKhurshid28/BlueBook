23a0726e4d960d12d1b6f5e7525b678b6a35f3c1
Sara Khurshid <Sara.Khurshid@zweidevs.com>
2023-08-03 17:31:34 +0500

    worksample component started

--- src/Components/WorkSample/WorkSample.jsx
+++ src/Components/WorkSample/WorkSample.jsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
 import { BlueFooter } from "../CommonComponents";
 import "./WorkSample.css";
 const WorkSample = () => {
+  const [isGen, setIsGen] = useState(true);
+  const [isDiv, setIsDiv] = useState(false);
+
+  const loadDivComp = () => {
+    return <div>Division</div>;
+  };
+
+  const loadGenComp = () => {
+    return <div>General</div>;
+  };
+
   return (
     <>
-      <div className="appJustifyAlignCenterItem" style={{ height: "49vh" }}>
-        Work Sample
+      <div className="wsMainContainer">
+        <div className="wsTabContainer">
+          <div
+            className={
+              isGen
+                ? "wsTabItemSelect wsTabItem"
+                : "wsTabItemUnSelect wsTabItem"
+            }
+            onClick={() => {
+              setIsGen(!isGen);
+              setIsDiv(!isDiv);
+            }}
+          >
+            General Samples
+          </div>
+          <div
+            className={
+              isDiv
+                ? "wsTabItemSelect wsTabItem"
+                : "wsTabItemUnSelect wsTabItem"
+            }
+            onClick={() => {
+              setIsGen(!isGen);
+              setIsDiv(!isDiv);
+            }}
+          >
+            Division Samples
+          </div>
+        </div>
+        <div> {isGen ? loadGenComp() : loadDivComp()}</div>
       </div>
       <BlueFooter />
     </>
